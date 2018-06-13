98f62b75d045538b4f7166696185efb8a8c8db9b
RRCCDataLab <adam.forland@rrcc.edu>
2018-06-12 18:22:18 -0600

    added (maybe not needed) predict code)

--- analysis/predict_new.py
+++ analysis/predict_new.py
@@ -1,9 +1,4 @@
 
-# coding: utf-8
-
-# In[1]:
-
-
 '''
 Code for assessing and sorting predictions made by keras model. Andy Cross and Adam Forland, Red Rocks Community
 College Data Lab, summer 2018
@@ -18,17 +13,10 @@ import glob
 import os
 
 
-# In[2]:
-
-
 #location of pretrained model
 model = load_model('/Users/andycross/Desktop/rgb_net_v4/rgb_small_1_test.h5')
 #folder of images to be predicted
 image_path = ('/Users/andycross/Desktop/test')
-
-
-# In[3]:
-
 
 #create folders for where to save sorted images
 if not os.path.exists("/Users/andycross/Desktop/animals"):
@@ -37,23 +25,18 @@ if not os.path.exists("/Users/andycross/Desktop/animals"):
 #create sub folders for sorting images        
 if not os.path.exists("/Users/andycross/Desktop/animals/cats"):
             os.makedirs("/Users/andycross/Desktop/animals/cats")
-        
+
 if not os.path.exists("/Users/andycross/Desktop/animals/dogs"):
             os.makedirs("/Users/andycross/Desktop/animals/dogs")
-        
+
 if not os.path.exists("/Users/andycross/Desktop/animals/fish"):
             os.makedirs("/Users/andycross/Desktop/animals/fish")
-        
-
-
-# In[5]:
-
 
 #code for making predictions and sorting predicted images
-def predict(loc,x_size,y_size):
-    i=0 
+def predict(loc ,x_size ,y_size):
+    i = 0
     for image in glob.glob(image_path+'/*.jpeg'):
-        img = cv2.imread(image,0) #read in images from glob
+        img = cv2.imread(image ,0) #read in images from glob
         img1 = cv2.resize(img,(150,113)) #give same shape as keras model was trained on 
         plt.imshow(img)
         img = img1.reshape(x_size,y_size,1) #reshape, last integer represents color channels
