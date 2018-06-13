0881ab8995584a2f6711321e0f56befde04d4a1c
Andy Cross <cross.andrew.r@gmail.com>
2018-06-13 00:15:21 -0600

    Add some networks

--- learning/base_CNN_arch.py
+++ learning/base_CNN_arch.py
@@ -1,5 +1,5 @@
 '''Base network architecture for multi classifiers. The data needs to come
-in as One Hot encoded already shuffled. 
+in as One Hot encoded already shuffled.
 Andy Cross and Adam Forland Red Rocks Community College, Data_Lab
 June 10, 2018'''
 
@@ -18,33 +18,35 @@ from keras.preprocessing import image
 from keras.models import load_model
 import matplotlib.pyplot as plt
 import time
+from os import getcwd
+import pickle
 
+mypath = getcwd()
 
 class centromere_trainer:
 
-    def prep_all_data():
-        all_data = np.load('all_data.npy')
-        x_train = all_data[:110,3:]
-        x_train = x_train.reshape(np.shape(x_train)[0],225,301,1)
+    def prep_all_data(self):
+        all_data = np.load(mypath + '\data_all.npy')
+        x_train = all_data[:900,3:]
+        x_train = x_train.reshape(np.shape(x_train)[0],113,150,1)
 
-        y_train = all_data[:110,:3]
-        y_test = all_data[110:,:3]
+        y_train = all_data[:900,:3]
+        y_test = all_data[900:,:3]
 
-        x_test = all_data[400:,3:]
-        x_test = x_test.reshape(np.shape(x_test)[0],225,301,1)
+        x_test = all_data[900:,3:]
+        x_test = x_test.reshape(np.shape(x_test)[0],113,150,1)
 
 
         train = x_train,y_train
-        validate = x_test,y_test
+        test = x_test,y_test
 
-        return(train)
-        return(test)
+        return(train,test)
 
-    def network_model():
+    def network_model(self):
         model = Sequential()
         model.add(Conv2D(32, kernel_size=(3, 3),
                          activation='relu',
-                         input_shape=(225,301,1)))
+                         input_shape=(113,150,1)))
         model.add(MaxPooling2D(pool_size=(3, 3)))
         model.add(Conv2D(32, (3, 3), activation='relu'))
         model.add(MaxPooling2D(pool_size=(3, 3)))
@@ -60,17 +62,16 @@ class centromere_trainer:
 
         return(model)
 
-    def network_train(model,train,validate):
+    def network_train(self,model,train,validate):
         history = model.fit(x = train[0], y = train[1],
             steps_per_epoch = 10,
             epochs = 7)
 
-        model.save('models/CNN_name_of_feature_+'+time.strftime("%Y-%m-%d") +'.h5')
-        with open('/History_name_of_feature' + time.strftime("%Y-%m-%d") , 'wb') as file_pi:
+        model.save('models/CNN_name_of_feature_' + time.strftime("%Y-%m-%d_%H-%M-%S") +'.h5')
+        with open('models/history/History_name_of_feature' + time.strftime("%Y-%m-%d_%H-%M-%S") , 'wb') as file_pi:
             pickle.dump(history.history, file_pi)
 
 cent = centromere_trainer()
 train , test = cent.prep_all_data()
 model = cent.network_model()
-network_train(model, train, test)
-
+cent.network_train(model, train, test)
