7361534bf27abbc81c54060a52c8ecb00a1010c7
RRCCDataLab <adam.forland@rrcc.edu>
2018-06-13 19:53:18 -0600

    base reads .h5 now

--- learning/base_CNN_arch.py
+++ learning/base_CNN_arch.py
@@ -8,7 +8,6 @@ import keras
 from keras import layers
 from keras.preprocessing.image import ImageDataGenerator
 from keras.datasets import mnist
-import cv2
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
@@ -16,8 +15,8 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
 from keras.preprocessing import image
 from keras.models import load_model
-import matplotlib.pyplot as plt
 import time
+import h5py
 from os import getcwd
 import pickle
 
@@ -27,15 +26,21 @@ print(mypath)
 class centromere_trainer:
 
     def prep_all_data(self):
-        all_data = np.load(mypath + '/preprocessing/all_data.npy')
-        x_train = all_data[:350,2:]
-        x_train = x_train.reshape(np.shape(x_train)[0],100,100,3)
+        
+        h5f = h5py.File('all_data.h5','r')
+        all_data = h5f['dataset_1'][:]
+        
+        print(np.shape(all_data))
 
+        x_train = all_data[:350,2:]
+        print(np.shape(x_train))
+        x_train = x_train.reshape(np.shape(x_train)[0],10,10,3)
+        print(np.shape(x_train))
         y_train = all_data[:350,:2]
         y_test = all_data[350:,:2]
 
         x_test = all_data[350:,2:]
-        x_test = x_test.reshape(np.shape(x_test)[0],100,100,3)
+        x_test = x_test.reshape(np.shape(x_test)[0],10,10,3)
 
 
         train = x_train,y_train
@@ -45,12 +50,12 @@ class centromere_trainer:
 
     def network_model(self):
         model = Sequential()
-        model.add(Conv2D(32, kernel_size=(3, 3),
+        model.add(Conv2D(32, kernel_size=(2, 2),
                          activation='relu',
-                         input_shape=(100,100,3)))
-        model.add(MaxPooling2D(pool_size=(3, 3)))
-        model.add(Conv2D(32, (3, 3), activation='relu'))
-        model.add(MaxPooling2D(pool_size=(3, 3)))
+                         input_shape=(10,10,3)))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Conv2D(32, (2, 2), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Dropout(0.25))
         model.add(Flatten())
         model.add(Dense(128, activation='relu'))
@@ -70,8 +75,8 @@ class centromere_trainer:
             steps_per_epoch = 10,
             epochs = 7)
 
-        model.save(mypath + '/learning/models/CNN_name_of_feature_' + time.strftime("%Y-%m-%d_%H-%M-%S") +'.h5')
-        with open(mypath + '/learning/models/history/History_name_of_feature' + time.strftime("%Y-%m-%d_%H-%M-%S") , 'wb') as file_pi:
+        model.save(mypath + '/models/CNN_name_of_feature_' + time.strftime("%Y-%m-%d_%H-%M-%S") +'.h5')
+        with open(mypath + '/models/history/History_name_of_feature' + time.strftime("%Y-%m-%d_%H-%M-%S") , 'wb') as file_pi:
             pickle.dump(history.history, file_pi)
 
 cent = centromere_trainer()
